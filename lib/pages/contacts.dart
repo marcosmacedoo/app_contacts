4eb09d6b250956992af04ae9b1f4a2f4e9ed868a
Marcos Macedo <marcosmacedoo.dev@gmail.com>
2023-06-04 22:25:12 -0300

    feat: create contacts list

--- lib/pages/contacts.dart
+++ lib/pages/contacts.dart
@@ -12,7 +12,41 @@ class ContactsScreen extends StatelessWidget {
         backgroundColor: Colors.yellow,
         centerTitle: true,
       ),
-      body: const Center(child: Text('Tela de contatos')),
+      floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          backgroundColor: Colors.yellow,
+          child: const Icon(Icons.add)),
+      body: Center(
+          child: ListView(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 20.0),
+        children: [
+          Card(
+              elevation: 0,
+              child: ListTile(
+                leading: const Icon(Icons.person),
+                title: const Text('Marcos Macedo'),
+                subtitle: const Text('(86) 9 9502-8103'),
+                iconColor: Theme.of(context).colorScheme.inverseSurface,
+                trailing: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.edit,
+                          color: Colors.indigo,
+                        )),
+                    IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.delete,
+                          color: Colors.red,
+                        )),
+                  ],
+                ),
+              ))
+        ],
+      )),
     );
   }
 }
